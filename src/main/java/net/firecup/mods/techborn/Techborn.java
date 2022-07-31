932165e35d0ae1a91a9f61ebb81df64dd5fd31fc
christianfirecup <christianfirecup@aol.com>
2022-07-31 02:49:48 -0400

    Added Titanium Ingot!!!

--- src/main/java/net/firecup/mods/techborn/Techborn.java
+++ src/main/java/net/firecup/mods/techborn/Techborn.java
@@ -1,10 +1,12 @@
 package net.firecup.mods.techborn;
 
 import com.mojang.logging.LogUtils;
+import net.firecup.mods.techborn.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -27,8 +29,11 @@ public class Techborn {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public Techborn() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+        eventBus.addListener(this::setup);
         // Register the enqueueIMC method for modloading
 
         // Register ourselves for server and other game events we are interested in
---
+++ src/main/java/net/firecup/mods/techborn/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.firecup.mods.techborn.item;
+
+import net.firecup.mods.techborn.Techborn;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Techborn.MOD_ID);
+
+    public static final RegistryObject<Item> TITANIUM = ITEMS.register("titanium_ingut",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
