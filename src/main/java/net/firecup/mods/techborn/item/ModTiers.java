bd2ad47cf7d615a2a34dfe5db3a4cdb6ff8a126a
christianfirecup <christianfirecup@aol.com>
2023-02-21 15:11:31 -0500

    fixed speed block development continues!

--- src/main/java/net/firecup/mods/techborn/item/ModTiers.java
+++ src/main/java/net/firecup/mods/techborn/item/ModTiers.java
@@ -1,11 +1,12 @@
 package net.firecup.mods.techborn.item;
 
+import net.firecup.mods.techborn.util.ModTags;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
 
 public class ModTiers {
-    public static final ForgeTier TITANIUM = new ForgeTier(4, 2000, 8f, 1f, 22, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier TITANIUM = new ForgeTier(4, 2000, 8f, 1f, 22, ModTags.Blocks.TITANIUM_PICKAXE,
             () -> Ingredient.of(ModItems.TITANIUM.get()));
 
 }
--- src/main/java/net/firecup/mods/techborn/item/PowerItem.java
+++ src/main/java/net/firecup/mods/techborn/item/PowerItem.java
@@ -1,16 +1,29 @@
 package net.firecup.mods.techborn.item;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
-
+import net.minecraft.world.level.Level;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 public class PowerItem extends Item {
     public PowerItem(Properties pProperties) {
         super(pProperties);
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext pContext) {
-        return super.useOn(pContext);
+    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
+        if(!pLevel.isClientSide()){
+
+        }
     }
 }
