a256ae678d3946ff9638d2f2f1c40a8cf79931c7
christianfirecup <christianfirecup@aol.com>
2024-06-25 20:39:36 -0400

    ore generation stuff

--- src/main/java/net/firecup/mods/techborn/features/ModConfigedFeatures.java
+++ src/main/java/net/firecup/mods/techborn/features/ModConfigedFeatures.java
@@ -9,24 +9,28 @@ import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.OreFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.placement.*;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 
 public class ModConfigedFeatures {
-    public static Holder<PlacedFeature> OW_OreGen;
+    public static Holder<PlacedFeature> OW_OreGentitanium;
+    public static Holder<PlacedFeature> OW_OreGenspeed;
 
     public static void registerOreFeatures() {
-        OreConfiguration owconfig = new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.TITANIUM_ORE.get().defaultBlockState(), 20);
-        OW_OreGen = registerPlacedOreFeature("titanium_ore", new ConfiguredFeature<>(Feature.ORE, owconfig));
+        OreConfiguration owconfigtitanium = new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.TITANIUM_ORE.get().defaultBlockState(), 20);
+        OreConfiguration owconfigspeed = new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.TITANIUM_ORE.get().defaultBlockState(), 20);
+        OW_OreGentitanium = registerPlacedOreFeature("titanium_ore", new ConfiguredFeature<>(Feature.ORE, owconfigtitanium),
                 CountPlacement.of(100),
                 InSquarePlacement.spread(),
                 BiomeFilter.biome(),
-                HeightRangePlacement.uniform(VerticalAnchor.absolute(0), VerticalAnchor.absolute(75)));
+                HeightRangePlacement.uniform(VerticalAnchor.absolute(-20), VerticalAnchor.absolute(75)));
+        OW_OreGenspeed= registerPlacedOreFeature("speed_block", new ConfiguredFeature<>(Feature.ORE, owconfigspeed),
+                CountPlacement.of(100),
+                InSquarePlacement.spread(),
+                BiomeFilter.biome(),
+                HeightRangePlacement.uniform(VerticalAnchor.absolute(-20), VerticalAnchor.absolute(75)));
 
     }
     private static <C extends FeatureConfiguration, F extends Feature<C>> Holder<PlacedFeature> registerPlacedOreFeature(String registryName, ConfiguredFeature<C, F> feature, PlacementModifier... placementModifiers) {
@@ -36,7 +40,7 @@ public class ModConfigedFeatures {
         if (event.getCategory() == Biome.BiomeCategory.NETHER) {
         } else if (event.getCategory() == Biome.BiomeCategory.THEEND) {
         } else {
-            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, OW_OreGen);
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, OW_OreGentitanium).addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, OW_OreGenspeed);
         }
 
 
